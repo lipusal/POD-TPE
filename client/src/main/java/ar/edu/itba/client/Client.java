d44693c0903ca9613d50f7ae7fb01dbfc1417978
Juan Li Puma <juan.lipuma@restorando.com>
2017-10-23 20:15:07 -0300

    Add base client and server classes

--- client/src/main/java/ar/edu/itba/client/Client.java
+++ client/src/main/java/ar/edu/itba/client/Client.java
@@ -1,5 +1,7 @@
 package ar.edu.itba.client;
 
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.core.HazelcastInstance;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -7,6 +9,8 @@ public class Client {
     private static Logger logger = LoggerFactory.getLogger(Client.class);
 
     public static void main(String[] args) {
-        logger.info("pod-tp Client Starting ...");
+        logger.info("Client starting ...");
+        final ClientConfig ccfg = new ClientConfig();
+        final HazelcastInstance hz = com.hazelcast.client.HazelcastClient.newHazelcastClient(ccfg);
     }
 }
--- server/src/main/java/ar/edu/itba/server/Server.java
+++ server/src/main/java/ar/edu/itba/server/Server.java
@@ -1,5 +1,8 @@
 package ar.edu.itba.server;
 
+import com.hazelcast.config.Config;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -7,6 +10,9 @@ public class Server {
     private static Logger logger = LoggerFactory.getLogger(Server.class);
 
     public static void main(String[] args) {
-        logger.info("pod-tp Server Starting ...");
+        logger.info("Node starting ...");
+
+        Config config = new Config();
+        HazelcastInstance h = Hazelcast.newHazelcastInstance(config);
     }
 }
