470ec84008d92dd182c5c2bbe4d005a686859198
Juan Li Puma <juan.lipuma@restorando.com>
2017-10-27 15:12:56 -0300

    Add Timer
    - Add timer class
    - Use timer methods in client

--- client/src/main/java/ar/edu/itba/client/Client.java
+++ client/src/main/java/ar/edu/itba/client/Client.java
@@ -3,19 +3,21 @@ package ar.edu.itba.client;
 import ar.edu.itba.Arguments;
 import ar.edu.itba.client.util.CsvParser;
 import com.beust.jcommander.JCommander;
+import ar.edu.itba.client.util.Timer;
 import com.hazelcast.client.config.ClientConfig;
 import com.hazelcast.core.HazelcastInstance;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
 public class Client {
     private static Logger logger = LoggerFactory.getLogger(Client.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         List<String> allArguments = Arguments.fromProperties(System.getProperties());
         allArguments.addAll(Arrays.asList(args));
 
@@ -25,13 +27,22 @@ public class Client {
                 .build()
                 .parse(allArguments.toArray(new String[0]));
         parsedArgs.postValidate();
-
         logger.info("Client starting ...");
         final ClientConfig ccfg = new ClientConfig();
         final HazelcastInstance hz = com.hazelcast.client.HazelcastClient.newHazelcastClient(ccfg);
 
-        // Curtanse tiene mi directorio
+        Timer timer = new Timer(new File( "time.txt"));
+
+        // Curtanse, tiene mi directorio
         CsvParser parser = new CsvParser(new File("/Users/juanlipuma/POD-TPE/census100.csv").toPath());
+        timer.dataReadStart();
         parser.parse().forEach(System.out::println);
+        timer.dataReadEnd();
+
+        // TODO upload data to cluster
+
+        timer.queryStart();
+        // TODO perform query
+        timer.queryEnd();
     }
 }
---
+++ client/src/main/java/ar/edu/itba/client/util/Timer.java
@@ -0,0 +1,101 @@
+package ar.edu.itba.client.util;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Class used to keep track of when certain events happened during the course of the program, in order to export them
+ * when the program ends.
+ */
+public class Timer {
+    private enum Status {NOT_STARTED, READING_DATA, READ_DATA, EXECUTING_QUERY, DONE}
+
+    private final FileWriter timeWriter;
+    private Status status;
+    private List<LocalDateTime> timestamps;
+
+    public Timer(File timeFile) throws IOException {
+        this.timeWriter = new FileWriter(timeFile);
+        this.timestamps = new ArrayList<>();
+        this.status = Status.NOT_STARTED;
+    }
+
+    /**
+     * Save data read start time.
+     *
+     * @throws IllegalStateException If called in an invalid state.
+     */
+    public void dataReadStart() throws IllegalStateException {
+        if (status != Status.NOT_STARTED) {
+            throw new IllegalStateException("dataReadStart() can only be called once, and before any other start/end methods");
+        }
+        saveTimestamp();
+        status = Status.READING_DATA;
+    }
+
+    /**
+     * Save data read end time.
+     *
+     * @throws IllegalStateException If called at any time other than after calling {@link #dataReadStart()}.
+     */
+    public void dataReadEnd() throws IllegalStateException {
+        if (status != Status.READING_DATA) {
+            throw new IllegalStateException("dataReadEnd() can only be called once, after dataReadStart()");
+        }
+        saveTimestamp();
+        status = Status.READ_DATA;
+    }
+
+    /**
+     * Save query start time.
+     *
+     * @throws IllegalStateException If called at any time other than after calling {@link #dataReadEnd()}
+     */
+    public void queryStart() throws IllegalStateException {
+        if (status != Status.READ_DATA) {
+            throw new IllegalStateException("queryStart() can only be called once, after dataReadEnd()");
+        }
+        saveTimestamp();
+        status = Status.EXECUTING_QUERY;
+    }
+
+    /**
+     * Save query end time, write all saved timestamps to file, and close it.
+     *
+     * @throws IllegalStateException If called at any time other than after calling {@link #queryStart()}
+     * @throws IOException           On I/O error with the time file.
+     */
+    public void queryEnd() throws IllegalStateException, IOException {
+        if (status != Status.EXECUTING_QUERY) {
+            throw new IllegalStateException("queryEnd() can only be called once, after queryStart()");
+        }
+        saveTimestamp();
+        status = Status.DONE;
+        writeTimestamps();
+        timeWriter.close();
+    }
+
+    /**
+     * Append the current timestamp to saved timestamps.
+     */
+    private void saveTimestamp() {
+        timestamps.add(LocalDateTime.now());
+    }
+
+    /**
+     * Write all saved timestamps in the time file, one per line.
+     */
+    private void writeTimestamps() {
+        timestamps.forEach(timestamp -> {
+            try {
+                timeWriter.write(timestamp + "\n");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+}
