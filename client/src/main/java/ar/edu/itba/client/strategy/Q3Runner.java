ef9c01fabaddce6f32422217d5f07552f28881e8
josevitali <jvitali@itba.edu.ar>
2017-11-04 19:23:40 -0300

    Add Q3Runner and change query3 calls in Client

---
+++ client/src/main/java/ar/edu/itba/client/strategy/Q3Runner.java
@@ -0,0 +1,72 @@
+package ar.edu.itba.client.strategy;
+
+import ar.edu.itba.CensusEntry;
+import ar.edu.itba.Region;
+import ar.edu.itba.client.util.ClientArguments;
+import ar.edu.itba.client.util.CsvParser;
+import ar.edu.itba.q3.CensusQuery3Collator;
+import ar.edu.itba.q3.CensusQuery3CombinerFactory;
+import ar.edu.itba.q3.CensusQuery3Mapper;
+import ar.edu.itba.q3.CensusQuery3ReducerFactory;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IList;
+import com.hazelcast.mapreduce.Job;
+import com.hazelcast.mapreduce.JobCompletableFuture;
+import com.hazelcast.mapreduce.KeyValueSource;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+
+public class Q3Runner extends BaseQueryRunner {
+
+    private IList<CensusEntry> iData;
+    private Map<Region, Double> result;
+
+    public Q3Runner(HazelcastInstance client, ClientArguments arguments) {
+        super(client, arguments);
+    }
+
+    @Override
+    public void readData() {
+        CsvParser parser = new CsvParser(arguments.getInFile().toPath());
+        data = parser.parse();
+    }
+
+    @Override
+    public void uploadData() {
+        iData = client.getList(getCollectionName());
+        iData.clear();
+        iData.addAll(data);
+    }
+
+    @Override
+    public void runQuery() throws ExecutionException, InterruptedException {
+        KeyValueSource<String, CensusEntry> keyValueSource = KeyValueSource.fromList(iData);
+        Job<String, CensusEntry> job = getJobTracker().newJob(keyValueSource);
+        JobCompletableFuture<Map<Region, Double>> future3 = job.mapper(new CensusQuery3Mapper()).combiner(new CensusQuery3CombinerFactory()).reducer(new CensusQuery3ReducerFactory()).submit(new CensusQuery3Collator());
+        result = future3.get();
+    }
+
+    @Override
+    public void writeResult() throws IOException {
+        FileWriter fw = new FileWriter(arguments.getOutFile());
+        fw.write(getResultString());
+        fw.close();
+    }
+
+    @Override
+    public void printResult() {
+        System.out.println(getResultString());
+    }
+
+    private String getResultString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Map.Entry<Region, Double> entry : result.entrySet()) {
+            stringBuilder.append(entry.getKey()).append(String.format(",%.2f\n", entry.getValue()));
+        }
+        return stringBuilder.toString();
+    }
+}
