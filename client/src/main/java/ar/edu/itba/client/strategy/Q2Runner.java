8fc931dd03a2adb28eed37cc5221659ff8560ba5
Juan Li Puma <juanlipuma94@gmail.com>
2017-11-04 22:13:27 -0300

    Implement Q2 runner

---
+++ client/src/main/java/ar/edu/itba/client/strategy/Q2Runner.java
@@ -0,0 +1,61 @@
+package ar.edu.itba.client.strategy;
+
+import ar.edu.itba.CensusEntry;
+import ar.edu.itba.client.util.ClientArguments;
+import ar.edu.itba.q2.CensusQuery2Collator;
+import ar.edu.itba.q2.CensusQuery2CombinerFactory;
+import ar.edu.itba.q2.CensusQuery2Mapper;
+import ar.edu.itba.q2.CensusQuery2ReducerFactory;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.mapreduce.KeyValueSource;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+
+public class Q2Runner extends BaseQueryRunner {
+    private List<Map.Entry<String, Integer>> result;
+
+    public Q2Runner(HazelcastInstance client, ClientArguments arguments) {
+        super(client, arguments);
+    }
+
+    @Override
+    public void runQuery() throws ExecutionException, InterruptedException {
+        KeyValueSource<String, CensusEntry> keyValueSource = KeyValueSource.fromList(iData);
+
+        result = getJobTracker().newJob(keyValueSource)
+                .mapper(new CensusQuery2Mapper(arguments.getProv()))
+                .combiner(new CensusQuery2CombinerFactory())
+                .reducer(new CensusQuery2ReducerFactory())
+                .submit(new CensusQuery2Collator(arguments.getN()))
+                .get();
+    }
+
+    @Override
+    public void writeResult() throws IOException {
+        FileWriter fw = new FileWriter(arguments.getOutFile());
+        fw.write(getResultString());
+        fw.close();
+    }
+
+    @Override
+    public void printResult() {
+        System.out.println(getResultString());
+    }
+
+    private String getResultString() {
+        StringBuilder builder = new StringBuilder();
+        Optional.ofNullable(result).orElse(Collections.emptyList()).forEach(stringIntegerEntry ->
+                builder.append(stringIntegerEntry.getKey())
+                        .append(",")
+                        .append(stringIntegerEntry.getValue())
+                        .append("\n")
+        );
+        return builder.toString();
+    }
+}
