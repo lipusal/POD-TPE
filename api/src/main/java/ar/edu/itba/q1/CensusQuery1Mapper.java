9aad2affb004068a32ccc2eadd271f526830deed
Juan Li Puma <juanlipuma94@gmail.com>
2017-11-05 10:50:29 -0300

    SOUT from different points of query 1 to make sure work is distributed

--- api/src/main/java/ar/edu/itba/q1/CensusQuery1Mapper.java
+++ api/src/main/java/ar/edu/itba/q1/CensusQuery1Mapper.java
@@ -5,8 +5,14 @@ import ar.edu.itba.Region;
 import com.hazelcast.mapreduce.Context;
 
 public class CensusQuery1Mapper implements com.hazelcast.mapreduce.Mapper<String, CensusEntry, Region, Integer> {
+
+    public CensusQuery1Mapper() {
+        System.out.println("Hello from query 1 mapper");
+    }
+
     @Override
     public void map(String key, CensusEntry censusEntry, Context<Region, Integer> context) {
+        System.out.println("Emitting from query q mapper");
         context.emit(censusEntry.getRegion(), 1);
     }
 }
--- api/src/main/java/ar/edu/itba/q1/CensusQuery1ReducerFactory.java
+++ api/src/main/java/ar/edu/itba/q1/CensusQuery1ReducerFactory.java
@@ -4,7 +4,12 @@ import ar.edu.itba.Region;
 import com.hazelcast.mapreduce.Reducer;
 import com.hazelcast.mapreduce.ReducerFactory;
 
-public class CensusQuery1ReducerFactory implements ReducerFactory<Region, Integer, Integer>{
+public class CensusQuery1ReducerFactory implements ReducerFactory<Region, Integer, Integer> {
+
+    public CensusQuery1ReducerFactory() {
+        System.out.println("Hello from query 1 reducer factory");
+    }
+
     @Override
     public Reducer<Integer, Integer> newReducer(Region region) {
         return new CensusQuery1Reducer();
@@ -15,11 +20,13 @@ public class CensusQuery1ReducerFactory implements ReducerFactory<Region, Intege
 
         @Override
         public void reduce(Integer value) {
+            System.out.println("Reducing from query 1 reducer");
             count++;
         }
 
         @Override
         public Integer finalizeReduce() {
+            System.out.println("Finalizing reduce from query 1 reducer");
             return count;
         }
     }
