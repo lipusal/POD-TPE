7a18d04d0ecab086d44c72660bf4d4ef82d6dbc1
Juan Li Puma <juan.lipuma@restorando.com>
2017-11-05 19:30:52 -0300

    Remove unused methods and fields in BaseQueryRunner

--- client/src/main/java/ar/edu/itba/client/strategy/BaseQueryRunner.java
+++ client/src/main/java/ar/edu/itba/client/strategy/BaseQueryRunner.java
@@ -1,46 +1,19 @@
 package ar.edu.itba.client.strategy;
 
-import ar.edu.itba.CensusEntry;
 import ar.edu.itba.client.util.ClientArguments;
-import ar.edu.itba.client.util.CsvParser;
 import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.core.IList;
-import com.hazelcast.core.IMap;
 import com.hazelcast.mapreduce.JobTracker;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Map;
 
 public abstract class BaseQueryRunner implements QueryRunner {
     protected final HazelcastInstance client;
     protected final ClientArguments arguments;
-    // TODO delete these, each runner should have its own typed collection
-    protected Map<Long, CensusEntry> dataMap;
-    protected IMap<Long, CensusEntry> iData;
 
     public BaseQueryRunner(HazelcastInstance client, ClientArguments arguments) {
         this.client = client;
         this.arguments = arguments;
-    }
-
-    /**
-     * Read data from file specified in arguments and save it as {@code List<CensusEntry>}.
-     */
-    @Override
-    public void readData() {
-        CsvParser parser = new CsvParser(arguments.getInFile().toPath());
-        this.dataMap = parser.parse();
-    }
-
-    /**
-     * Upload data read from {@link #readData()} as an {@link IList}.
-     */
-    @Override
-    public void uploadData() {
-        iData = client.getMap(getCollectionName());
-        iData.clear();
-        iData.putAll(dataMap);
     }
 
     @Override
